ed0840f3cb87f66631d7f9e4276f01b6177482fd
Charles Kerr <charles@charleskerr.com>
2023-09-08 16:26:06 -0500

    refactor: use updated variant API in rpcimpl

--- libtransmission/utils.h
+++ libtransmission/utils.h
@@ -20,6 +20,7 @@
 #include "libtransmission/tr-macros.h"
 
 struct tr_error;
+struct tr_variant;
 
 /**
  * @addtogroup utils Utilities
@@ -325,7 +326,7 @@ extern uint64_t tr_size_K; /* unused? */
 /** @brief Format a file size from bytes into a user-readable string. */
 [[nodiscard]] std::string tr_formatter_size_B(uint64_t bytes);
 
-struct tr_variant tr_formatter_get_units();
+tr_variant tr_formatter_get_units();
 
 [[nodiscard]] static inline size_t tr_toSpeedBytes(size_t KBps)
 {
